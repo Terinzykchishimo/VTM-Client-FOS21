41efdda93f7f00ed62cda1355cd527f8638135f6
dima kizima <dima8kizima@gmail.com>
2026-03-17 21:21:35 +0100

    Der Code wurde geschrieben

---
+++ VTM-Client-FOS21/IModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VTM_Client_FOS21
+{
+    internal interface IModel
+    {
+        string data { get; set; }
+        void reader();
+        void writer();
+        
+    }
+}
